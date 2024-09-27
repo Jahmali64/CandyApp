a23a417feb930d9026c6dbde42a0fe7cfd7c08b8
Jahmali Mcknight <jahmali@rampinteractive.onmicrosoft.com>
2024-09-26 18:38:20 -0600

    started making login system

---
+++ CandyApp.Application/ViewModels/AuthorizedUserVM.cs
@@ -0,0 +1,7 @@
+﻿namespace CandyApp.Application.ViewModels;
+
+public sealed class AuthorizedUserVM {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
---
+++ CandyApp.UI/Models/UserRegistrationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CandyApp.UI.Models;
+
+public record class UserRegistrationViewModel {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
